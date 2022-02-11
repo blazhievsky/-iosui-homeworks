d974aa52eb9c9acaf96383817bc7ce540a7a039b
blazhievsky <blazhievsky007@ya.ru>
2022-02-11 13:53:37 +0300

    Initial Commit

---
+++ Navagation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navagation
+//
+//  Created by Artem Blazhievsky on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
